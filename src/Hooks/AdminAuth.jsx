f6ed9caa35db02d652ea82c2221a74a0e1fa8d98
khalidumar29 <dreamiwoarld.online@gmail.com>
2023-11-25 02:58:58 +0600

    loading error fix

--- src/Hooks/AdminAuth.jsx
+++ src/Hooks/AdminAuth.jsx
@@ -4,32 +4,47 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../firebase.init";
 import Loading from "../Components/Loading";
 import { Navigate, useLocation } from "react-router-dom";
+
 const AdminAuth = ({ children }) => {
   const [user] = useAuthState(auth);
   const [isAdmin, setIsAdmin] = useState(false);
   const [loading, setLoading] = useState(true);
   const location = useLocation();
+
   useEffect(() => {
     const fetchAdmin = async () => {
-      const userStatus = await useFetchApproveUser(user?.email);
-      if (userStatus === "Admin") {
-        setIsAdmin(true);
-      } else {
-        setIsAdmin(false);
+      try {
+        const userStatus = await useFetchApproveUser(user?.email);
+        if (userStatus === "Admin") {
+          setIsAdmin(true);
+        } else {
+          setIsAdmin(false);
+        }
+      } catch (error) {
+        console.error("Error fetching admin status:", error);
+      } finally {
+        setLoading(false);
       }
     };
-    fetchAdmin();
-    setLoading(false);
+
+    if (user) {
+      fetchAdmin();
+    } else {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    }
   }, [user]);
 
-  if (loading || !isAdmin) {
+  if (loading) {
     return <Loading></Loading>;
   }
 
   if (!isAdmin && !loading) {
     return (
       <h1 className='text-2xl font-bold'>
-        status:409, message: 'unauthorized acess!'
+        status:409, message: 'unauthorized access!'
       </h1>
     );
   }
