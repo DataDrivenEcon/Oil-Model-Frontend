a637d2a1d93b68cf31fd950a676df8863349748d
DataDrivenEcon <khalid100umar@gmail.com>
2024-02-02 17:06:10 +0600

    pie chart add done

--- src/Components/OurProducts.jsx
+++ src/Components/OurProducts.jsx
@@ -1,58 +1,43 @@
 import React from "react";
-import { IoStorefrontOutline } from "react-icons/io5";
-import { GiMedicines, GiParkBench } from "react-icons/gi";
-import { FaBusAlt } from "react-icons/fa";
-import { HiMiniBuildingOffice2 } from "react-icons/hi2";
+import PieChartComponents from "./Chart/PieChartComponents";
 const OurProducts = () => {
-  const products = [
-    {
-      name: "retail stores and recreation",
-      icon: <IoStorefrontOutline className='w-full h-full p-2 text-white' />,
-    },
-    {
-      name: "supermarkets and pharmacies",
-      icon: <GiMedicines className='w-full h-full p-2 text-white' />,
-    },
-    {
-      name: "parks",
-      icon: <GiParkBench className='w-full h-full p-2 text-white' />,
-    },
-    {
-      name: "public transport",
-      icon: <FaBusAlt className='w-full h-full p-2 text-white' />,
-    },
-    {
-      name: "work",
-      icon: <HiMiniBuildingOffice2 className='w-full h-full p-2 text-white' />,
-    },
-  ];
   return (
-    <div className='flex flex-col bg-gradient-to-r from-slate-500 to-slate-800'>
-      <h1 className='text-5xl p-8 text-white font-semibold'>
+    <div className='flex flex-col items-center bg-gradient-to-r from-slate-500 to-slate-800 py-10'>
+      <h1 className='text-5xl text-white font-semibold'>
         Our Product: Road Mobility
       </h1>
-      <p className='text-white text-xl pl-8 pr-24 font-medium '>
+      <p className='text-white text-center text-xl pt-5 px-28'>
+        {" "}
         Road Mobility is a community-based method for estimating both weekly and
         monthly distances traveled by cars in the U.S. By tapping into online
         social media, Road Mobility is able to track five different motives for
-        travelling.
-        <br />
-        When combined, their sum roughly equals total vehicle miles traveled
-        (VMT) as reported by the U.S. Department of Transportation.
+        travelling: 1) driving to retail stores and recreation 2) driving to
+        supermarkets and pharmacies 3) driving to parks 4) driving to public
+        transport and 5) driving to work.
+        <ul className='flex gap-10 justify-center items-center pt-3 capitalize'>
+          <li className='list-disc marker:text-[#0088FE]'>Work</li>
+          <li className='list-disc marker:text-[#00C49F]'>
+            retail and recreation
+          </li>
+          <li className='list-disc marker:text-[#FFBB28]'>
+            supermarkets and pharmacies
+          </li>
+          <li className='list-disc marker:text-[#FF8042]'>others</li>
+        </ul>
       </p>
-      <div className='flex flex-wrap justify-center items-center gap-3 py-20'>
-        {products.map((product, i) => (
-          <div
-            key={i}
-            className='flex flex-col justify-center items-center gap-2 shadow-sm border-2 border-neutral-500-400 p-5 rounded w-96'
-          >
-            <div className='w-20 h-20 rounded-full'>{product.icon}</div>
-            <h1 className='text-xl text-white font-medium capitalize'>
-              {product.name}
-            </h1>
-          </div>
-        ))}
-      </div>
+      <PieChartComponents />
+      <p className='text-white text-xl text-center px-28'>
+        When combined, their sum roughly equals total vehicle miles traveled
+        (VMT) as reported by the U.S. Department of Transportation. This is a
+        significant finding because the published numbers are themselves
+        estimates. In addition, our methodology allows us to analyze driving
+        trends by end-use at a state/regional and national level. In addition,
+        Road Mobility’s data are more current. As shown in the following chart,
+        these motivations for driving have varied since 2020 with the latest
+        readings indicating that driving to work constitutes 19% of total VMT
+        followed by driving to retail stores and recreation at 34% and driving
+        to supermarkets and pharmacies at 37%.
+      </p>
     </div>
   );
 };
